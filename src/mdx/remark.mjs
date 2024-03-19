796879a6d7bbe81070b38884d0311b500efa79bc
zhuyudong <zhuyudong@aliyun.com>
2024-03-19 22:46:56 +0800

    feat: support mdx frontmatter

--- src/mdx/remark.mjs
+++ src/mdx/remark.mjs
@@ -1,4 +1,16 @@
 import { mdxAnnotations } from 'mdx-annotations'
 import remarkGfm from 'remark-gfm'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
-export const remarkPlugins = [mdxAnnotations.remark, remarkGfm]
+export const remarkPlugins = [
+  mdxAnnotations.remark /**
+   * NOTE: support for frontmatter in MDX files
+   * ---
+   * title: Hello, world!
+   * ---
+   */,
+  remarkFrontmatter,
+  remarkMdxFrontmatter,
+  remarkGfm
+]
