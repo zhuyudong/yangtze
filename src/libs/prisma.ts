4b59f88a365d1e8266a465a45bf94f02a785bab2
Yudong Zhu <yudong8.zhu@gmail.com>
2024-04-07 18:47:22 +0800

    feat: ✨ prisma

--- src/libs/prisma.ts
+++ src/libs/prisma.ts
@@ -1,21 +1,25 @@
 /* eslint-disable import/no-mutable-exports */
 import { PrismaClient } from '@prisma/client'
 
-// let prisma: PrismaClient
+let prisma: PrismaClient
 
-// if (process.env.NODE_ENV === 'production') {
-//   prisma = new PrismaClient()
-// } else {
-//   if (!global.prisma) {
-//     global.prisma = new PrismaClient()
-//   }
-//   prisma = global.prisma
-// }
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  // NOTE: global.prisma 类型在 src/types/global.d.ts 中定义
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
 
 // export default prisma
+export { prisma }
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+// 等效于如下
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+// const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+// export const prisma = globalForPrisma.prisma || new PrismaClient()
+
+// if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,6 +1,19 @@
+/* eslint-disable vars-on-top */
+/* eslint-disable no-var */
 /* eslint-disable @typescript-eslint/consistent-type-imports */
 // Use type safe message keys with `next-intl`
+// import { PrismaClient } from '@prisma/client'
 import { type SearchOptions } from 'flexsearch'
+
+// NOTE: 这里使用 declare global {} 来声明全局变量
+declare global {
+  namespace globalThis {
+    // NOTE: 注意这里使用 var 而不是 let 或 const
+    var prisma: import('@prisma/client').PrismaClient
+    // or
+    // var prisma: PrismaClient
+  }
+}
 
 type Messages = typeof import('../locales/en.json')
 declare interface IntlMessages extends Messages {}
