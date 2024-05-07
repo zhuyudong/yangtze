79c5419e2be7e5353cd5fe1c7e8d2520d5af3083
zhuyudong <zhuyudong@aliyun.com>
2024-05-07 23:45:05 +0800

    chore: 🔧 format

--- src/lib/highlight-code.ts
+++ src/lib/highlight-code.ts
@@ -1,13 +1,13 @@
-"use server"
+'use server'
 
-import { promises as fs } from "fs"
-import path from "path"
-import { getHighlighter } from "shiki"
+import { promises as fs } from 'fs'
+import path from 'path'
+import { getHighlighter } from 'shiki'
 
 // Highlighting is failing in server components.
 // Disabling this in development.
 // TODO: Remove this when we figure out the issue.
-const highlightCodeEnabled = process.env.NODE_ENV !== "development"
+const highlightCodeEnabled = process.env.NODE_ENV !== 'development'
 
 export async function highlightCode(code: string) {
   if (!highlightCodeEnabled) {
@@ -15,20 +15,20 @@ export async function highlightCode(code: string) {
   }
 
   const editorTheme = await fs.readFile(
-    path.join(process.cwd(), "lib/themes/dark.json"),
-    "utf-8"
+    path.join(process.cwd(), 'lib/themes/dark.json'),
+    'utf-8'
   )
 
   const highlighter = await getHighlighter({
-    langs: ["typescript"],
-    themes: [],
+    langs: ['typescript'],
+    themes: []
   })
 
   await highlighter.loadTheme(JSON.parse(editorTheme))
 
   const html = await highlighter.codeToHtml(code, {
-    lang: "typescript",
-    theme: "Lambda Studio — Blackout",
+    lang: 'typescript',
+    theme: 'Lambda Studio — Blackout'
   })
 
   return html
